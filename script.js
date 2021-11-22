bbff5b4c7b1f219caff3ba99c5d1128dadf374d4
Rachel Tairum <rachel.tairum@gmail.com>
2021-11-22 16:28:57 -0300

    Correção requisito 2 e requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let local = document.querySelector('#pixel-board');
 
-for(let i = 0; i <= 5; i += 1){
-    let pixel = document.createElement('div');
-    pixel.className = 'pixel'
-    local.appendChild(pixel)
-
+for(let i = 0; i < 5; i += 1){
+    for(let index = 0; index < 5; index += 1){
+        let pixel = document.createElement('div');
+        pixel.className = 'pixel'
+        local.appendChild(pixel)
+    }   
+    let quebra = document.createElement('br');
+    local.appendChild(quebra)
 }
